315faeb9354555379f5ba25dfef7e89fc02a298f
montes8 <emontesinos812@gmail.com>
2018-09-24 21:07:08 -0500

    creando activitis a usar

---
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/NavegacionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tayler_gabbi.demo_monedero
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NavegacionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navegacion)
+    }
+}
