64c2f83fb4918168399b286186ef42131b0a4d8c
montes8 <emontesinos812@gmail.com>
2018-09-29 11:03:38 -0500

    implementando activitymain

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/MainActivity.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.tayler_gabbi.demo_monedero
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_sesion.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_register.setOnClickListener{
+            val intent = Intent(this, RegistrarActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
