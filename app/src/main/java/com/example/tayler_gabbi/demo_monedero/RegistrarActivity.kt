0e89c3059a95048c11da1facdff89d10f8993dc3
montes8 <emontesinos812@gmail.com>
2018-09-30 16:11:25 -0500

    creando las funciones de insertar usuario

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/RegistrarActivity.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/RegistrarActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import com.example.tayler_gabbi.demo_monedero.model.Usuario
 import kotlinx.android.synthetic.main.activity_registrar.*
 import org.jetbrains.anko.*
 
@@ -26,42 +27,37 @@ class RegistrarActivity : AppCompatActivity() {
     }
 
     private fun registrarUsuario() {
-        button_click_register.setOnClickListener {
+        button_register_ingresar.setOnClickListener {
 
 
             Thread {
 
-                if (edit_text_nombre.text.toString().isEmpty()) {
+                if (edit_text_name.text.toString().isEmpty()) {
 
                     handler.post { toast("ingrese nombre") }
 
-                }else if(edit_text_user.text.toString().isEmpty()){
+                }else if(edit_text_usuario.text.toString().isEmpty()){
                     handler.post { toast("Ingrese nombre de usuario") }
 
-                }else if(edit_password.text.toString().isEmpty()){
+                }else if(edit_text_password.text.toString().isEmpty()){
                     handler.post { toast("Ingrese contraseña") }
 
-                }else if(edit_pais.text.toString().isEmpty()){
-                    handler.post { toast("Ingrese pais") }
                 }else {
                     val usuario = Usuario()
-                    usuario.nombre = edit_text_nombre.text.toString()
-                    usuario.nombreUsuario = edit_text_user.text.toString()
-                    usuario.contrasenia = edit_password.text.toString()
-                    usuario.pais = edit_pais.text.toString()
+                    usuario.nombre = edit_text_name.text.toString()
+                    usuario.nombreUsuario = edit_text_usuario.text.toString()
+                    usuario.contrasenia = edit_text_password.text.toString()
 
                     val nuevoId = DemoApplication.database!!.usuarioDao().insert(usuario)
-                    defaultSharedPreferences.edit().putLong("idUsuarioLogeado",nuevoId).apply()
                     if (nuevoId > 0) {
                         Log.i("idregistrado", "$nuevoId")
                         handler.post {
                             toast("Usuario Registrado")
-                            edit_text_nombre.setText("")
-                            edit_text_user.setText("")
-                            edit_password.setText("")
-                            edit_pais.setText("")
+                            edit_text_name.setText("")
+                            edit_text_usuario.setText("")
+                            edit_text_password.setText("")
 
-                            startActivity(intentFor<CategoriaActivity>().newTask().clearTask())
+                            startActivity(intentFor<NavegacionActivity>().newTask().clearTask())
                         }
                     } else {
                         handler.post { toast("errorrrr") }
