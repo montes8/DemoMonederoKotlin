0d59ce629e10c034a663441c2824ca65f357c764
montes8 <emontesinos812@gmail.com>
2018-10-01 20:01:47 -0500

    creando splash

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/SplashActivity.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/SplashActivity.kt
@@ -1,12 +1,59 @@
 package com.example.tayler_gabbi.demo_monedero
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.example.tayler_gabbi.demo_monedero.view.NavegacionActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+import org.jetbrains.anko.defaultSharedPreferences
+import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val anima : Animation
+        val anima2 : Animation
+
+        anima = AnimationUtils.loadAnimation(this, R.anim.animacion)
+        anima2 = AnimationUtils.loadAnimation(this, R.anim.animacion_botton)
+        once.animation=anima
+        doce.animation=anima2
+
+
+        llamarSplash()
     }
+
+
+
+
+
+fun llamarSplash (){
+    val background = object : Thread() {
+        override fun run() {
+            try {
+                Thread.sleep((3*1000).toLong())
+                val idUsuarioLogeado = defaultSharedPreferences.getLong("idUsuarioLogeado",0)
+
+                if (idUsuarioLogeado > 0){
+
+                    startActivity<NavegacionActivity>()
+                }else{
+                    val i = Intent(baseContext, MainActivity::class.java)
+                    startActivity(i)
+                }
+
+                finish()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }
+    }
+    background.start()
+}
 }
