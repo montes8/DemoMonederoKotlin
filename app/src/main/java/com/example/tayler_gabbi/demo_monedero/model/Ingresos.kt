465f409a5ba9d836e73fe0a5f44c3b3081feab33
montes8 <emontesinos812@gmail.com>
2018-10-06 08:43:35 -0500

    creando model de ingresos

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/model/Ingresos.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/model/Ingresos.kt
@@ -1,4 +1,21 @@
 package com.example.tayler_gabbi.demo_monedero.model
 
-class Ingresos {
-}
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+
+@Entity
+class Ingresos (
+        @PrimaryKey(autoGenerate = true)
+        var IdIngreso:Int?=null,
+
+        var descripcion:String?="",
+
+        var monto:Int?=0,
+
+        var dia:String?="",
+
+        var mes:String?="",
+
+        var anio:String?=""
+)
