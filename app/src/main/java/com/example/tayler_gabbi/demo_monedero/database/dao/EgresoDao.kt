c0d8ff8b713a53c59613cc0d28b65dc8e5c6be53
montes8 <emontesinos812@gmail.com>
2018-10-11 08:01:11 -0500

    crenado interfaz egresosDao

---
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/database/dao/EgresoDao.kt
@@ -0,0 +1,21 @@
+package com.example.tayler_gabbi.demo_monedero.database.dao
+
+import android.arch.persistence.room.*
+import com.example.tayler_gabbi.demo_monedero.model.Egresos
+import com.example.tayler_gabbi.demo_monedero.model.Usuario
+
+@Dao
+interface EgresoDao {
+
+    @Query("select * from Egresos")
+    fun listarUsuario(): List<Usuario>
+
+    @Insert
+    fun insert(egreso: Egresos): Long
+
+    @Delete
+    fun deleteUser(egreso: Egresos):Int
+
+    @Update
+    fun updateUser(egreso: Egresos)
+}
