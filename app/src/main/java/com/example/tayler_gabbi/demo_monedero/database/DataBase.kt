627ff538fde1d6b4d5321ab53e9b22ab1446a896
montes8 <emontesinos812@gmail.com>
2018-10-11 08:02:09 -0500

    creando tabla egresos

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/database/DataBase.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/database/DataBase.kt
@@ -3,10 +3,11 @@ package com.example.tayler_gabbi.demo_monedero.database
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 import com.example.tayler_gabbi.demo_monedero.database.dao.UsuarioDao
+import com.example.tayler_gabbi.demo_monedero.model.Egresos
 import com.example.tayler_gabbi.demo_monedero.model.Usuario
 
 
-@Database(entities = [Usuario::class],version = 1)
+@Database(entities = [Usuario::class,Egresos::class],version = 1)
 abstract class DataBase :RoomDatabase(){
 
     abstract fun usuarioDao() : UsuarioDao
