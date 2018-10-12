9c11c0c8311177b6d3a9baf93fa0ba611233f621
montes8 <emontesinos812@gmail.com>
2018-10-12 08:53:37 -0500

    agregado a la bd la tabla categoria

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/database/DataBase.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/database/DataBase.kt
@@ -4,11 +4,12 @@ import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 import com.example.tayler_gabbi.demo_monedero.database.dao.EgresoDao
 import com.example.tayler_gabbi.demo_monedero.database.dao.UsuarioDao
+import com.example.tayler_gabbi.demo_monedero.model.Categoria
 import com.example.tayler_gabbi.demo_monedero.model.Egresos
 import com.example.tayler_gabbi.demo_monedero.model.Usuario
 
 
-@Database(entities = [Usuario::class,Egresos::class],version = 1)
+@Database(entities = [Usuario::class,Egresos::class,Categoria::class],version = 1)
 abstract class DataBase :RoomDatabase(){
 
     abstract fun usuarioDao() : UsuarioDao
