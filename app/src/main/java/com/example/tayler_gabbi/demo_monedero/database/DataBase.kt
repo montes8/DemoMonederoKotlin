1e23fe2536e96f128d01624a85972a14044dea5f
montes8 <emontesinos812@gmail.com>
2018-10-13 08:14:59 -0500

    agregando tabla ingresos a la bd

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/database/DataBase.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/database/DataBase.kt
@@ -7,10 +7,11 @@ import com.example.tayler_gabbi.demo_monedero.database.dao.EgresoDao
 import com.example.tayler_gabbi.demo_monedero.database.dao.UsuarioDao
 import com.example.tayler_gabbi.demo_monedero.model.Categoria
 import com.example.tayler_gabbi.demo_monedero.model.Egresos
+import com.example.tayler_gabbi.demo_monedero.model.Ingresos
 import com.example.tayler_gabbi.demo_monedero.model.Usuario
 
 
-@Database(entities = [Usuario::class,Egresos::class,Categoria::class],version = 1)
+@Database(entities = [Usuario::class,Egresos::class,Categoria::class,Ingresos::class],version = 1)
 abstract class DataBase :RoomDatabase(){
 
     abstract fun usuarioDao() : UsuarioDao
