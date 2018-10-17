3c4c76f3aed4a28ad4a6ea5aa99b725349b50487
montes8 <emontesinos812@gmail.com>
2018-10-17 09:20:24 -0500

    agregando toolbar a listaCategoria

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/listasView/ListaCategoria.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/listasView/ListaCategoria.kt
@@ -3,11 +3,23 @@ package com.example.tayler_gabbi.demo_monedero.listasView
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tayler_gabbi.demo_monedero.R
+import kotlinx.android.synthetic.main.activity_lista_categoria.*
 
 class ListaCategoria : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_categoria)
+
+
+    }
+
+    fun ajusteToolbar(){
+
+        setSupportActionBar(toolbar_lista_categoria)
+        toolbar_lista_categoria.navigationIcon = getDrawable(R.drawable.ic_atras)
+        toolbar_lista_categoria.setOnClickListener{
+            onBackPressed()
+        }
     }
 }
