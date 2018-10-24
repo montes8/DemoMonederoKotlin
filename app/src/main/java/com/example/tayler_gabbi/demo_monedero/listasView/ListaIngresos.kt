b17951c75d7e382346e199f7d9849745662b9631
montes8 <emontesinos812@gmail.com>
2018-10-24 00:21:35 -0500

    agregando toolbar a lista de usuarios

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/listasView/ListaIngresos.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/listasView/ListaIngresos.kt
@@ -3,8 +3,8 @@ package com.example.tayler_gabbi.demo_monedero.listasView
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tayler_gabbi.demo_monedero.R
-import kotlinx.android.synthetic.main.activity_lista_egresos.*
 import kotlinx.android.synthetic.main.activity_lista_ingresos.*
+import kotlinx.android.synthetic.main.activity_lista_usuario.*
 
 class ListaIngresos : AppCompatActivity() {
 
@@ -16,10 +16,10 @@ class ListaIngresos : AppCompatActivity() {
     }
 
     fun ajustarToolbarIngreso(){
-        setSupportActionBar(toolbar_ingreso)
+        setSupportActionBar(toolbar_usuario)
         title = "Lista de Ingresos"
-        toolbar_ingreso.navigationIcon = getDrawable(R.drawable.ic_atras)
-        toolbar_ingreso.setOnClickListener{
+        toolbar_usuario.navigationIcon = getDrawable(R.drawable.ic_atras)
+        toolbar_usuario.setOnClickListener{
             onBackPressed()
         }
 
--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/listasView/ListaUsuario.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/listasView/ListaUsuario.kt
@@ -3,11 +3,25 @@ package com.example.tayler_gabbi.demo_monedero.listasView
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tayler_gabbi.demo_monedero.R
+import kotlinx.android.synthetic.main.activity_lista_ingresos.*
 
 class ListaUsuario : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_usuario)
+
+        ajustarToolbarUsuario()
+    }
+
+
+    fun ajustarToolbarUsuario(){
+        setSupportActionBar(toolbar_ingreso)
+        title = "Lista de Ingresos"
+        toolbar_ingreso.navigationIcon = getDrawable(R.drawable.ic_atras)
+        toolbar_ingreso.setOnClickListener{
+            onBackPressed()
+        }
+
     }
 }
