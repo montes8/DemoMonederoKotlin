34a2e23c158d0ea1064fb5d824b8fca510fe1003
montes8 <emontesinos812@gmail.com>
2018-10-29 20:43:51 -0500

    agregando funcion al boton ver categoria

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/fragments/ConfiguracionFragment.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/fragments/ConfiguracionFragment.kt
@@ -1,12 +1,14 @@
 package com.example.tayler_gabbi.demo_monedero.fragments
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tayler_gabbi.demo_monedero.R
+import com.example.tayler_gabbi.demo_monedero.listasView.ListaCategoria
 import com.example.tayler_gabbi.demo_monedero.listasView.ListaUsuario
 import com.example.tayler_gabbi.demo_monedero.view.CategoriaActivity
 import org.jetbrains.anko.support.v4.startActivity
@@ -31,7 +33,11 @@ class ConfiguracionFragment : Fragment(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v!!.id) {
             R.id.button_categoria -> startActivity<CategoriaActivity>()
-            R.id.ver_categoria -> startActivity<ListaUsuario>()
+            R.id.ver_usuarios -> startActivity<ListaUsuario>()
+            R.id.ver_categoria -> {
+                val intent = Intent(context,ListaCategoria::class.java)
+                startActivity(intent)
+            }
 
         }
     }
