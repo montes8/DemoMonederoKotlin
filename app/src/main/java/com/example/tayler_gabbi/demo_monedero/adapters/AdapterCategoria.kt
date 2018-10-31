43ca37c16f35439f58ea97f86542097cafec674a
montes8 <emontesinos812@gmail.com>
2018-10-30 21:35:31 -0500

    creando variabe privada categorias y creando metodo agregar lista

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/adapters/AdapterCategoria.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/adapters/AdapterCategoria.kt
@@ -3,10 +3,19 @@ package com.example.tayler_gabbi.demo_monedero.adapters
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.example.tayler_gabbi.demo_monedero.model.Categoria
 import kotlinx.android.synthetic.main.molde_categoria.view.*
 
 class AdapterCategoria : RecyclerView.Adapter<AdapterCategoria.CategoriaViewHolder>(){
 
+    private var categorias : List<Categoria>? = null
+
+    fun agregarLista (categorias : List<Categoria>){
+        this.categorias = categorias
+
+        notifyDataSetChanged()
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriaViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
