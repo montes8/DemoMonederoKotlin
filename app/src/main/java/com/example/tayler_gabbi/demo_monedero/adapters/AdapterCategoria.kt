b029de560b42f7462505217d296f43053276354a
montes8 <emontesinos812@gmail.com>
2018-10-30 21:41:24 -0500

    sobreescribiendo metodos del recyclerView,inflando el los item del molde

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/adapters/AdapterCategoria.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/adapters/AdapterCategoria.kt
@@ -1,8 +1,10 @@
 package com.example.tayler_gabbi.demo_monedero.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tayler_gabbi.demo_monedero.R
 import com.example.tayler_gabbi.demo_monedero.model.Categoria
 import kotlinx.android.synthetic.main.molde_categoria.view.*
 
@@ -18,11 +20,14 @@ class AdapterCategoria : RecyclerView.Adapter<AdapterCategoria.CategoriaViewHold
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriaViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view : View = LayoutInflater.from(parent.context).inflate(R.layout.molde_categoria,parent,false)
+        return CategoriaViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val checkedUser = checkNotNull(categorias){return 0}
+
+        return checkedUser.size
     }
 
     override fun onBindViewHolder(holder: CategoriaViewHolder, position: Int) {
