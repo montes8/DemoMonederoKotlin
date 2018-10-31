681169e65f476e1d57d4b86bf33107e1467aa5c9
montes8 <emontesinos812@gmail.com>
2018-10-30 21:28:51 -0500

    creando clase anidada categoriaViewHodel en el adapter

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/adapters/AdapterCategoria.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/adapters/AdapterCategoria.kt
@@ -1,4 +1,32 @@
 package com.example.tayler_gabbi.demo_monedero.adapters
 
-class AdapterCategoria {
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.molde_categoria.view.*
+
+class AdapterCategoria : RecyclerView.Adapter<AdapterCategoria.CategoriaViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriaViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: CategoriaViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    class CategoriaViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+        val categoria = itemView.text_view_categoria
+        val eliminarcategoria = itemView.image_delete_categoria
+        val editarCategoria = itemView.image_edit_categoria
+
+    }
+
 }
