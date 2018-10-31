cde6eb5793f9105b20090301496b0378723cf259
montes8 <emontesinos812@gmail.com>
2018-10-31 00:57:45 -0500

    creando adapter de la lista de usuarios

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/adapters/AdapterUsuario.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/adapters/AdapterUsuario.kt
@@ -1,4 +1,60 @@
 package com.example.tayler_gabbi.demo_monedero.adapters
 
-class AdapterUsuario {
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tayler_gabbi.demo_monedero.R
+import com.example.tayler_gabbi.demo_monedero.model.Categoria
+import com.example.tayler_gabbi.demo_monedero.model.Usuario
+import kotlinx.android.synthetic.main.molde_categoria.view.*
+import kotlinx.android.synthetic.main.molde_list_user.view.*
+
+class AdapterUsuario ( val onClickEditarUsuario : ((Usuario) -> Unit)? = null, val onClickEliminarUsuario : ((Usuario) -> Unit)? = null ): RecyclerView.Adapter<AdapterUsuario.UsuarioViewHolder>(){
+
+    private var usuarios : List<Usuario>? = null
+
+    fun agregarLista (usuarios : List<Usuario>){
+        this.usuarios = usuarios
+
+        notifyDataSetChanged()
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsuarioViewHolder {
+        val view : View = LayoutInflater.from(parent.context).inflate(R.layout.molde_categoria,parent,false)
+        return UsuarioViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        val checkedUser = checkNotNull(usuarios){return 0}
+
+        return checkedUser.size
+    }
+
+    override fun onBindViewHolder(holder: UsuarioViewHolder, position: Int) {
+        val usuario = usuarios!![position]
+
+        holder.nombre.text = usuario.nombre
+        holder.nombreusuario.text = usuario.nombreUsuario
+        holder.contrasenia.text = usuario.contrasenia
+
+
+        holder.eliminarcategoria.setOnClickListener {
+
+            onClickEliminarUsuario?.invoke(usuario)
+        }
+
+
+    }
+
+
+    class UsuarioViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+        val nombre = itemView.text_view_nombre1
+        val nombreusuario = itemView.text_view_usuario1
+        val contrasenia = itemView.text_view_password1
+        val eliminarcategoria = itemView.image_delete_categoria
+
+    }
 }
