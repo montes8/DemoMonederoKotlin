3f8604b03c28d8f7cd072f99d34f2589e19c3c7a
montes8 <emontesinos812@gmail.com>
2018-11-03 06:31:50 -0500

    mostrando lista en la vista

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/listasView/ListaUsuario.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/listasView/ListaUsuario.kt
@@ -2,17 +2,41 @@ package com.example.tayler_gabbi.demo_monedero.listasView
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.support.v7.widget.LinearLayoutManager
+import com.example.tayler_gabbi.demo_monedero.DemoApplication
 import com.example.tayler_gabbi.demo_monedero.R
+import com.example.tayler_gabbi.demo_monedero.adapters.AdapterCategoria
+import com.example.tayler_gabbi.demo_monedero.adapters.AdapterUsuario
+import kotlinx.android.synthetic.main.activity_lista_categoria.*
 import kotlinx.android.synthetic.main.activity_lista_ingresos.*
 import kotlinx.android.synthetic.main.activity_lista_usuario.*
+import kotlin.concurrent.thread
 
 class ListaUsuario : AppCompatActivity() {
+
+    var usuarioAdapter : AdapterUsuario? = null
+    var handler : Handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_usuario)
 
         ajustarToolbarUsuario()
+
+        usuarioAdapter = AdapterUsuario()
+
+        lista_categoria.layoutManager = LinearLayoutManager(this)
+        lista_categoria.adapter = usuarioAdapter
+
+        thread(start = true){
+            val lista = DemoApplication.database!!.usuarioDao().listarUsuario()
+            handler.post {
+
+                usuarioAdapter?.agregarLista(lista)
+            }
+
+        }
     }
 
 
