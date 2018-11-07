0d97598db077c016c9e3bc2487af3979c52d1bd6
montes8 <emontesinos812@gmail.com>
2018-11-07 12:57:55 -0500

    inicializando grafico

--- app/src/main/java/com/example/tayler_gabbi/demo_monedero/GraficoActivity.kt
+++ app/src/main/java/com/example/tayler_gabbi/demo_monedero/GraficoActivity.kt
@@ -2,11 +2,16 @@ package com.example.tayler_gabbi.demo_monedero
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.github.mikephil.charting.charts.LineChart
 
 class GraficoActivity : AppCompatActivity() {
+
+    lateinit var chart: LineChart
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grafico)
+
+        chart=findViewById<LineChart>(R.id.chart)
     }
 }
